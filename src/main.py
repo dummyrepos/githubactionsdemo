6e27a13117831f6aaea90ec7dae911ec68977afc
shaikkhajaibrahim <qtkhajadevops@gmail.com>
2024-11-05 20:31:22 +0530

    Added changes

--- src/main.py
+++ src/main.py
@@ -1,1 +1,60 @@
-# This file is main application file
+# This class will have all the methods for the calculator
+class Calculator:
+    # This method will add two numbers
+    def add(self, a, b):
+        return a + b
+
+    # This method will subtract two numbers
+    def subtract(self, a, b):
+        return a - b
+
+    # This method will multiply two numbers
+    def multiply(self, a, b):
+        return a * b
+
+    # This method will divide two numbers
+    def divide(self, a, b):
+        return a / b
+
+# This is the main function that will run the calculator
+def main():
+    # Create an instance of the Calculator class
+    calc = Calculator()
+
+    # Print the menu
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+    print("5. Exit")
+
+    # Get the user's choice
+    choice = int(input("Enter your choice: "))
+
+    # Loop until the user chooses to exit
+    while choice != 5:
+        # Get the two numbers from the user
+        a = int(input("Enter the first number: "))
+        b = int(input("Enter the second number: "))
+
+        # Perform the chosen operation
+        if choice == 1:
+            print("Result:", calc.add(a, b))
+        elif choice == 2:
+            print("Result:", calc.subtract(a, b))
+        elif choice == 3:
+            print("Result:", calc.multiply(a, b))
+        elif choice == 4:
+            print("Result:", calc.divide(a, b))
+        else:
+            print("Invalid choice")
+
+        # Get the user's choice again
+        choice = int(input("Enter your choice: "))
+
+    # Print a goodbye message
+    print("Goodbye!")
+
+# This is the entry point of the program
+if __name__ == "__main__":
+    main()
